9c65d69ac7aa873a0fbe7e28d97a697dd3b99cc2
Pratik Valvi <pratikvalvi@pop-os.localdomain>
2023-08-26 15:10:19 +0530

    Added padding to search bar

--- app/Components/Header.tsx
+++ app/Components/Header.tsx
@@ -20,7 +20,7 @@ const Header = () => {
           height={100}
           className='w-44 md:w-56 pb-10 md:pb-0 object-contain'
         />
-        <div className='flex w-full items-center justify-end space-x-5 flex-1'>
+        <div className='flex w-full items-center justify-end space-x-5 flex-1 py-2'>
           <form className='flex items-center justify-center space-x-5 bg-white rounded-md p-2 shadow-md flex-1 md:flex-initial'>
             <MagnifyingGlassIcon className='h-6 w-6 text-gray-400' />
             <input value={searchString} onChange={(e) => setSearchString(e.target.value)} type="text" placeholder='Search' className='flex-1 outline-none p-2' />
